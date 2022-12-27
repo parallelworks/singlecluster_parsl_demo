5218375018aba354b42a39493fe6e2c90f4f401a
avidalto <avidalto@hawk.iit.edu>
2022-12-27 08:25:54 -0600

    Use wrapper only to call parsl_utils.sh

--- parsl_wrapper.sh
+++ parsl_wrapper.sh
@@ -4,7 +4,7 @@ set -x
 # Can't get workflow type in PW to clone --recurse-submodules
 #rm -rf parsl_utils
 #git clone https://github.com/parallelworks/parsl_utils.git parsl_utils
-cp parsl_utils/kill.sh .
-cp parsl_utils/main.sh .
+#cp parsl_utils/kill.sh .
+#cp parsl_utils/main.sh .
 #
-bash main.sh $@
+bash parsl_utils/main.sh $@
