a5a92085cedbdc906119d96a76ea960e7e21c613
avidalto <avidalto@hawk.iit.edu>
2022-12-27 06:17:22 -0600

    Removing the git clone to test --recurse-submodules in pw

--- parsl_wrapper.sh
+++ parsl_wrapper.sh
@@ -2,8 +2,9 @@
 #!/bin/bash
 set -x
 # Can't get workflow type in PW to clone --recurse-submodules
-rm -rf parsl_utils
-git clone https://github.com/parallelworks/parsl_utils.git parsl_utils
+#rm -rf parsl_utils
+#git clone https://github.com/parallelworks/parsl_utils.git parsl_utils
 cp parsl_utils/kill.sh .
 cp parsl_utils/main.sh .
+#
 bash main.sh $@
