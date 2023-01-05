bb59b646b4553908f5b7d5ddf390a32784424548
Alvaro Vidal Torreira <avidalto@hawk.iit.edu>
2023-01-05 07:35:32 -0600

    Otherwise the submodule is fixed to a given commit...

--- parsl_wrapper.sh
+++ parsl_wrapper.sh
@@ -2,6 +2,7 @@
 set -x
 
 # Otherwise the submodule is fixed to a given commit...
-git submodule update --remote 
+rm -rf parsl_utils
+git clone https://github.com/parallelworks/parsl_utils.git parsl_utils
 # Cant run a scripts inside parsl_utils directly
 bash parsl_utils/main.sh $@
