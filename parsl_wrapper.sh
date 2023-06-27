5bab0451564a58e9495bcb5b2d3ba6a6e0908ea1
avidalto <avidalto@hawk.iit.edu>
2023-06-27 17:18:32 -0500

    Adapt example to new workflow framework

--- parsl_wrapper.sh
+++ parsl_wrapper.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-set -x
+source /etc/profile.d/parallelworks.sh
+source /etc/profile.d/parallelworks-env.sh
+source /pw/.miniconda3/etc/profile.d/conda.sh
+conda activate
+
+python input_form_resource_wrapper.py
 
 # Otherwise the submodule is fixed to a given commit...
 rm -rf parsl_utils
-git clone -b dev https://github.com/parallelworks/parsl_utils.git parsl_utils
+git clone -b new-workflow https://github.com/parallelworks/parsl_utils.git parsl_utils
 # Cant run a scripts inside parsl_utils directly
-bash parsl_utils/main.sh $@
+bash parsl_utils/main.sh
