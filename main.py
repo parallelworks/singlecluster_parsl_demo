f1c569af5cc03cfca86768cb76428a4836fa3f3a
avidalto <avidalto@hawk.iit.edu>
2023-06-28 11:22:41 -0500

    Get form_inputs from config.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from parsl.app.app import python_app
 print(parsl.__version__, flush = True)
 
 import parsl_utils
-from parsl_utils.config import config, resource_labels
+from parsl_utils.config import config, resource_labels, inputs_dict
 
 from workflow_apps import hello_executor
 
@@ -14,16 +14,13 @@ if __name__ == '__main__':
     print('Loading Parsl Config', flush = True)
     parsl.load(config)
 
-    with open('inputs.json') as f:
-        inputs = json.load(f)
-
     # List to store the futures of the hello_executor app
     hello_executor_futs = []
 
     for label in resource_labels:
         print(f'\nRunning job in executor {label}')
         hello_executor_futs.append(
-            python_app(hello_executor, executors=[label])(inputs['greeting'])
+            python_app(hello_executor, executors=[label])(inputs_dict['greeting'])
         )
     
     for li,label in enumerate(resource_labels):
