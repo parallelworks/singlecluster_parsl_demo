d7787538ae515848dbd5d2a37c4ab0c3ef2f8be3
Alvaro Vidal Torreira <avidalto@hawk.iit.edu>
2023-10-27 03:32:06 -0500

    Update parsl_wrapper.sh

--- parsl_wrapper.sh
+++ parsl_wrapper.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Otherwise the submodule is fixed to a given commit...
 rm -rf parsl_utils
-git clone https://github.com/parallelworks/parsl_utils.git parsl_utils
+
+git clone -b test-pwp-address https://github.com/stefangary/parsl_utils.git parsl_utils
 # Cant run a scripts inside parsl_utils directly
 bash parsl_utils/main.sh
